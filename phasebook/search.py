2177d3cee22f01b7ac7a7809afa3cd573820604c
JustineLopez <jus.paololopez@gmail.com>
2024-05-29 10:15:39 +0800

    Added search_users function

--- phasebook/search.py
+++ phasebook/search.py
@@ -10,7 +10,6 @@ bp = Blueprint("search", __name__, url_prefix="/search")
 def search():
     return search_users(request.args.to_dict()), 200
 
-
 def search_users(args):
     """Search users database
 
@@ -26,5 +25,40 @@ def search_users(args):
     """
 
     # Implement search here!
+    id = args.get('id')
+    name = args.get('name')
+    age = args.get('age')
+    occupation = args.get('occupation')
 
-    return USERS
+    if name:
+        name = name.lower()
+    if occupation:
+        occupation = occupation.lower()
+    if age:
+        try:
+            age = int(age)
+        except ValueError:
+            return []  # Invalid nteger return empty list
+
+    result = []
+    for user in USERS:
+        
+        if id is not None and str(user.get('id')) == id:
+            result.append(user)
+            continue
+        if name is not None and name in user.get('name', '').lower():
+            result.append(user)
+            continue
+        if age is not None and (age - 1 <= user.get('age', age) <= age + 1):
+            result.append(user)
+            continue
+        if occupation is not None and occupation in user.get('occupation', '').lower():
+            result.append(user)
+            continue
+
+    unique_result = {user['id']: user for user in result}.values() # remove duplicates
+
+    
+        # user['id'], user['name'], user['age'], user[occupation]))
+
+    return list(unique_result)
